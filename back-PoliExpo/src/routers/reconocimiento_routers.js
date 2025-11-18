f5337f9af4d491051f719a1aafc0bb30368d5ff7
Xavier <luis.ochoa02@epn.edu.ec>
2025-11-18 15:57:37 -0500

    Delete back-PoliExpo directory

--- back-PoliExpo/src/routers/reconocimiento_routers.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { reconocerRostro } from "../controllers/reconocimiento_controller.js";
-
-const router = Router();
-
-router.post("/reconocer", reconocerRostro);
-
-export default router;
