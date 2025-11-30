f1064b6a8a75d35b59bfabf299c9a1ac2a1bf4cc
Bryan Angulo <andiubra@gmail.com>
2025-11-30 15:59:46 -0500

    cambio estilo uniformizacion login register forgot

--- front-PoliExpo/src/pages/Reset.jsx
+++ front-PoliExpo/src/pages/Reset.jsx
@@ -1,100 +1,92 @@
-import logoDog from '../assets/dog-hand.webp'
-import { useState } from 'react'
-import { useEffect } from 'react'
-import {useFetch} from '../hooks/useFetch';
-import { useNavigate, useParams } from 'react-router'
-
-import { ToastContainer } from 'react-toastify'
-
-import { useForm } from 'react-hook-form'
-
+import personHand from '../assets/person-hand.webp';
+import { useState, useEffect } from 'react';
+import { useFetch } from '../hooks/useFetch';
+import { useNavigate, useParams } from 'react-router';
+import { ToastContainer } from 'react-toastify';
+import { useForm } from 'react-hook-form';
+import logoPoliExpo from '../assets/logo-PoliExpo3.png';
 
 const Reset = () => {
-
-    const navigate = useNavigate()
-
-    const { token } = useParams()
-    const  fetchDataBackend  = useFetch()
+    const navigate = useNavigate();
+    const { token } = useParams();
+    const fetchDataBackend = useFetch();
     const [tokenback, setTokenBack] = useState(false);
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
-    const { register, handleSubmit, formState: { errors } } = useForm()
-
-
-     const changePassword = async (dataForm) => {
-        const url = `${import.meta.env.VITE_BACKEND_URL}/nuevopassword/${token}`
-        await fetchDataBackend(url, dataForm,'POST')
+    const changePassword = async (dataForm) => {
+        const url = `${import.meta.env.VITE_BACKEND_URL}/nuevopassword/${token}`;
+        await fetchDataBackend(url, dataForm, 'POST');
         setTimeout(() => {
             if (dataForm.password === dataForm.confirmpassword) {
-                navigate('/login')
+                navigate('/login');
             }
-        }, 2000)
-    }
+        }, 2000);
+    };
 
     useEffect(() => {
-        const verifyToken = async()=>{
-            const url = `${import.meta.env.VITE_BACKEND_URL}/recuperarpassword/${token}`
-            await fetchDataBackend(url,'GET')
-            setTokenBack(true)
-        }
-        verifyToken()
-    }, [])
-    
+        const verifyToken = async () => {
+            const url = `${import.meta.env.VITE_BACKEND_URL}/recuperarpassword/${token}`;
+            await fetchDataBackend(url, 'GET');
+            setTokenBack(true);
+        };
+        verifyToken();
+    }, []);
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-red-600 to-black p-4">
             <ToastContainer />
-            
-            <h1 className="text-3xl font-semibold mb-2 text-center text-gray-500">
-                Bienvenido nuevamente
-            </h1>
-            <small className="text-gray-400 block my-4 text-sm">
-                Pro favor, ingrese los siguientes datos
-            </small>
-            <img
-                className="object-cover h-80 w-80 rounded-full border-4 border-solid border-slate-600"
-                src={logoDog}
-                alt="image description"
-            />
+            <div className="bg-white rounded-xl shadow-lg p-8 flex flex-col items-center w-full max-w-md">
+                <div className="flex flex-col items-center gap-4 mb-6">
+                    <img src={logoPoliExpo} alt="logo" className="w-30 h-30" />
+                    <h1 className="text-4xl font-bold text-gray-800 text-center">Bienvenido</h1>
+                </div>
 
-            {tokenback && (
+                <h1 className="text-2xl font-semibold mb-2 text-center text-gray-500">
+                    Restablece tu contraseña
+                </h1>
+                <small className="text-gray-400 block my-4 text-sm text-center">
+                    Por favor, ingrese los siguientes datos
+                </small>
+                <img
+                    className="object-cover h-40 w-40 rounded-full border-4 border-solid border-slate-600 mb-6"
+                    src={personHand}
+                    alt="image description"
+                />
 
-                 <form className="w-80" onSubmit={handleSubmit(changePassword )}>
+                {tokenback && (
+                    <form className="w-full" onSubmit={handleSubmit(changePassword)}>
+                        <div className="mb-4">
+                            <label className="mb-2 block text-sm font-semibold">Nueva contraseña</label>
+                            <input
+                                type="password"
+                                placeholder="Ingresa tu nueva contraseña"
+                                className="block w-full rounded-md border border-gray-300 py-2 px-3 text-gray-700"
+                                {...register("password", { required: "La contraseña es obligatoria" })}
+                            />
+                            {errors.password && <p className="text-red-600 mt-1">{errors.password.message}</p>}
 
-                    <div className="mb-1">
+                            <label className="mb-2 block mt-2 text-sm font-semibold">Confirmar contraseña</label>
+                            <input
+                                type="password"
+                                placeholder="Repite tu contraseña"
+                                className="block w-full rounded-md border border-gray-300 py-2 px-3 text-gray-700"
+                                {...register("confirmpassword", { required: "La contraseña es obligatoria" })}
+                            />
+                            {errors.confirmpassword && <p className="text-red-800 mt-1">{errors.confirmpassword.message}</p>}
+                        </div>
 
-                        {/* Campo nueva contraseña */}
-                        <label className="mb-2 block text-sm font-semibold">Nueva contraseña</label>
-                        <input type="password" placeholder="Ingresa tu nueva contraseña"
-                            className="block w-full rounded-md border border-gray-300 py-1 px-1.5 text-gray-500"
-                        {...register("password", { required: "La contraseña es obligatoria" })}
-                        />
-                            {errors.password && <p className="text-red-800">{errors.password.message}</p>}
-                        
-                        
-                        {/* Campo repetir contraseña */}
-                        <label className="mb-2 block text-sm font-semibold">Confirmar contraseña</label>
-                        <input type="password" placeholder="Repite tu contraseña"
-                            className="block w-full rounded-md border border-gray-300 py-1 px-1.5 text-gray-500"
-                        {...register("confirmpassword", { required: "La contraseña es obligatoria" })}
-                        />
-                            {errors.confirmpassword && <p className="text-red-800">{errors.confirmpassword.message}</p>}
-
-
-                    </div>
-
-                    <div className="mb-3">
-                        <button className="bg-gray-600 text-slate-300 border py-2 
-                        w-full rounded-xl mt-5 hover:scale-105 duration-300 hover:bg-gray-900 
-                        hover:text-white">
-                            Enviar
-                        </button>
-
-                    </div>
-                    
-                </form>
-            )}
+                        <div className="mb-3">
+                            <button
+                                className="bg-black -600 text-slate-300 w-full py-2 rounded-xl mt-5 hover:scale-105 duration-300 hover:bg-red-700 hover:text-white"
+                            >
+                                Enviar
+                            </button>
+                        </div>
+                    </form>
+                )}
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Reset
+export default Reset;
