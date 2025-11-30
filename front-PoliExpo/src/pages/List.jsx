6225551cc6945cc6e3ffb0eb1ad2120e74d7f770
Bryan Angulo <andiubra@gmail.com>
2025-11-30 12:29:17 -0500

    Estilos dashboard

--- front-PoliExpo/src/pages/List.jsx
+++ front-PoliExpo/src/pages/List.jsx
@@ -3,9 +3,9 @@ import Table from "../components/list/Table"
 const List = () => {
     return (
         <div>
-            <h1 className='font-black text-4xl text-gray-500'>Listar</h1>
+            <h1 className='font-black text-4xl text-gray-500'>Mis Publicaciones</h1>
             <hr className='my-4 border-t-2 border-gray-300' />
-            <p className='mb-8'>Este módulo te permite gestionar registros existentes</p>
+            <p className='mb-8'>Este módulo te permite gestionar tus publicaciones existentes</p>
             <Table/>
         </div>
     )
