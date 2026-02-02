a68192ed40b613629f133625ca11beab2d9aa8d8
Bryan Angulo <andiubra@gmail.com>
2026-02-02 00:13:22 -0500

    chat

--- back-PoliExpo/src/index.js
+++ back-PoliExpo/src/index.js
@@ -1,41 +1,29 @@
-/*import app from './server.js'
-import connection from './database.js';
-
-connection()
-
-app.listen(app.get('port'),()=>{
-    console.log(`Server ok on http://localhost:${app.get('port')}`);
-})*/
-
+import 'dotenv/config'
 import app from './server.js'
 import connection from './database.js'
 import http from 'http'
 import { Server } from 'socket.io'
-
 
 connection()
 
 const server = http.createServer(app)
 
 const io = new Server(server, {
-    cors: {
-        origin: process.env.URL_FRONTEND,
-        methods: ["GET", "POST"],
-        credentials: true
-    }
+  cors: {
+    origin: process.env.URL_FRONTEND,
+    methods: ["GET", "POST"],
+    credentials: true
+  }
 })
-
-
 
 io.on('connection', (socket) => {
-    console.log('Usuario conectado',socket.id)
-    socket.on('enviar-mensaje-front-back',(payload)=>{
-        socket.broadcast.emit('enviar-mensaje-front-back',payload)
-    })
+  console.log('Usuario conectado', socket.id)
+
+  socket.on('enviar-mensaje-front-back', (payload) => {
+    socket.broadcast.emit('enviar-mensaje-front-back', payload)
+  })
 })
 
-
-
-server.listen(app.get('port'),()=>{
-    console.log(`Server ok on http://localhost:${app.get('port')}`);
+server.listen(app.get('port'), () => {
+  console.log(`Server ok on http://localhost:${app.get('port')}`)
 })
